9d1cc7faf065be95aff1f737a1f680d58c838d4c
axbybgl <77483537+axbybgl@users.noreply.github.com>
2022-10-21 20:28:56 +0800

    Update Dataset.py

--- Dataset/Dataset.py
+++ Dataset/Dataset.py
@@ -15,7 +15,7 @@ for i in range(len(yHatList)):
     yiHat = yHatList[i]
     yi = yList[i]
     gi = -yi * (1 - 1 / (1 + math.pow(math.e, -yiHat))) + (1 - yi) / (1 + math.pow(math.e, -yiHat))
-    hi =  math.pow(math.e, -yiHat) / math.pow(math.e, -yiHat) ** 2
+    hi =  math.pow(math.e, -yiHat) / (1 + math.pow(math.e, -yiHat)) ** 2
     gList.append(gi)
     HList.append(hi)
 
